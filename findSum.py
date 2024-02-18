63ea61f0dfd0747a6b673096e1b06955c3da6468
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-02-18 07:47:34 +0600

    sum of numbers that are divisible by 3 not by 5

--- findSum.py
+++ findSum.py
@@ -1,15 +1,17 @@
 def findSum(startRange, endRange):
     sum = 0
 
-    print("Calculated Elements: ",end="")
+    print("Calculated Elements: ", end="")
 
     for i in range(startRange, endRange):
-        if(i%3==0 and i%5!=0):
-            print(f"{i} ",end="")
-            sum+=i
-    print(f"\nAddition Result: {sum}",end="")
+        if i % 3 == 0 and i % 5 != 0:
+            print(f"{i} ", end="")
+            sum += i
+    print(f"\nAddition Result: {sum}", end="")
+
 
 def main():
-    findSum(1,20)
+    findSum(1, 20)
+
 
 main()
