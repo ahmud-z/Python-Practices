7673db662ba6d76923953953bcc2d57c04e4f80c
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-02-21 20:18:53 +0600

    updated

--- sum of array elements.py
+++ sum of array elements.py
@@ -1,14 +1,17 @@
-def find_sum(arr):
+def find_sum(arrayList):
     sum = 0
-    for i in range(0, len(arr)):
-        sum = sum+i
-        
+
+    print("Given numbers: ", end="")
+    for i in arrayList:
+        print(f"{i} ", end="")
+        sum = sum + i
+
     return sum
 
-         
-def main():
-    arr = [1,2,3,4,5,6,7,8,9,10]
-    print("sum of array element: "+str(find_sum(arr)))
 
+def main():
+    arrayList = [6,12,7,1,10]
+    sum = find_sum(arrayList)
+    print(f"\nSum Result: {sum}")
 
 main()
